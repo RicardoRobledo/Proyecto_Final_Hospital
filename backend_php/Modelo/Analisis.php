39a673a3da26fd529a7742da0b8aab388ad0f89f
RicardoRobledo <99063411+RicardoRobledo@users.noreply.github.com>
2022-10-21 19:37:09 -0500

    fix(Modelo): Se corrigieron errores
    Se corrigieron errores en la clase 'Analisis'

--- backend_php/Modelo/Analisis.php
+++ backend_php/Modelo/Analisis.php
@@ -3,13 +3,23 @@
 class Analisis
 {
 
+    private $id_analisis;
 	private $id_paciente;
 	private $tipo_analisis;
 	
-	public function __construct($id_paciente=null, $tipo_analisis=null)
+	public function __construct($id_analisis=null, $id_paciente=null, $tipo_analisis=null)
 	{
+		$this->id_analisis = $id_analisis;
 		$this->id_paciente = $id_paciente;
 		$this->tipo_analisis = $tipo_analisis;
+	}
+
+	public function getIdAnalisis(){
+		return $this->id_analisis;
+	}
+
+	public function setIdAnalisis($id_paciente){
+		$this->id_analisis = $id_analisis;
 	}
 
 	public function getIdPaciente(){
