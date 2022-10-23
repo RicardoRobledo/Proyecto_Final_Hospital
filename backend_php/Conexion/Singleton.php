0eafa9ac65f712b2a370c4626bf11ec794503c3b
RicardoRobledo <99063411+RicardoRobledo@users.noreply.github.com>
2022-10-22 21:39:31 -0500

    feat(Conexion): Se modifico el archivo 'Singleton.php'
    Se agrego el crud para pacientes

--- backend_php/Conexion/Singleton.php
+++ backend_php/Conexion/Singleton.php
@@ -34,6 +34,8 @@ class Singleton
             new Singleton();
         }
 
+        print('Conexion obtenida');
+
         return Singleton::$conexion;
 		
     }
@@ -48,7 +50,7 @@ class Singleton
 
     
     # ----------------------------------------------------------------
-    #                         Inicio de sesion
+    #                             Usuario
     # ----------------------------------------------------------------
     public static function iniciarSesion($nombre_usuario, $contrasenia)
     {
@@ -63,16 +65,206 @@ class Singleton
 
         //Si la consulta se realiza con exito
     	if($query->fetch(PDO::FETCH_ASSOC)['sp_login']==''){
-    		return "NO";
+    		return false;
     	}else{
-    		return "SI";
+    		return true;
     	}
 
     }
 
+
+    # ----------------------------------------------------------------
+    #                             Paciente
+    # ----------------------------------------------------------------
+    public static function altaPaciente($paciente)
+    {
+
+        $sql = 'INSERT INTO pacientes(nombre, apellido_paterno, apellido_materno, num_telefono, edad, sexo, direccion) VALUES (?,?,?,?,?,?,?)';
+
+        $query = Singleton::$conexion->prepare($sql);
+        Singleton::$conexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $query->bindValue(1, $paciente->getNombre(), PDO::PARAM_STR);
+        $query->bindValue(2, $paciente->getApellidoPaterno(), PDO::PARAM_STR);
+        $query->bindValue(3, $paciente->getApellidoMaterno(), PDO::PARAM_STR);
+        $query->bindValue(4, $paciente->getNumTelefono(), PDO::PARAM_STR);
+        $query->bindValue(5, $paciente->getEdad(), PDO::PARAM_INT);
+        $query->bindValue(6, $paciente->getSexo(), PDO::PARAM_STR);
+        $query->bindValue(7, $paciente->getDireccion(), PDO::PARAM_STR);
+        $resultado = $query->execute();
+
+        if($resultado){
+            return true; 
+        }else{
+            return false;
+        }
+
+    }
+
+
+    public static function bajaPaciente($paciente)
+    {
+
+        $sql = 'DELETE FROM pacientes WHERE id_paciente=?';
+
+        $query = Singleton::$conexion->prepare($sql);
+        Singleton::$conexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $query->bindValue(1, $paciente->getIdPaciente(), PDO::PARAM_INT);
+        $resultado = $query->execute();
+
+        if($resultado){
+            return true; 
+        }else{
+            return false;
+        }
+
+    }
+
+
+    public static function cambioPaciente($paciente)
+    {
+
+        $sql = 'UPDATE pacientes SET nombre=?, apellido_paterno=?, apellido_materno=?, num_telefono=?, edad=?, sexo=?, direccion=? WHERE id_paciente=?';
+
+        $query = Singleton::$conexion->prepare($sql);
+        Singleton::$conexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $query->bindValue(1, $paciente->getNombre(), PDO::PARAM_STR);
+        $query->bindValue(2, $paciente->getApellidoPaterno(), PDO::PARAM_STR);
+        $query->bindValue(3, $paciente->getApellidoMaterno(), PDO::PARAM_STR);
+        $query->bindValue(4, $paciente->getNumTelefono(), PDO::PARAM_STR);
+        $query->bindValue(5, $paciente->getEdad(), PDO::PARAM_INT);
+        $query->bindValue(6, $paciente->getSexo(), PDO::PARAM_STR);
+        $query->bindValue(7, $paciente->getDireccion(), PDO::PARAM_STR);
+        $query->bindValue(8, $paciente->getIdPaciente(), PDO::PARAM_INT);
+        $resultado = $query->execute();
+
+        if($resultado){
+            return true; 
+        }else{
+            return false;
+        }
+
+    }
+
+
+    public static function generarConsulta($paciente)
+    {
+
+        $sql = 'SELECT * FROM pacientes WHERE ';
+
+        if($paciente->getIdPaciente()!=null){
+            $sql = $sql . 'id_paciente=? AND ';
+        }
+
+        if($paciente->getNombre()!=null){
+            $sql = $sql . 'nombre=? AND ';
+        }
+
+        if($paciente->getApellidoPaterno()!=null){
+            $sql = $sql . 'apellido_paterno=? AND ';
+        }
+
+        if($paciente->getApellidoMaterno()!=null){
+            $sql = $sql . 'apellido_materno=? AND ';
+        }
+
+        if($paciente->getNumTelefono()!=null){
+            $sql = $sql . 'num_telefono=? AND ';
+        }
+
+        if($paciente->getEdad()!=null){
+            $sql = $sql . 'edad=? AND ';
+        }
+
+        if($paciente->getSexo()!=null){
+            $sql = $sql . 'sexo=? AND ';
+        }
+
+        if($paciente->getDireccion()!=null){
+            $sql = $sql . 'direccion=? AND ';
+        }
+
+        return $sql;
+
+    }
+
+
+    public static function consultar($sql, $paciente)
+    {
+
+        $cont = 1;
+        $query = Singleton::$conexion->prepare(substr($sql, 0, strlen($sql)-5));
+
+        Singleton::$conexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        if($paciente->getIdPaciente()!=null){
+            $query->bindValue($cont, $paciente->getIdPaciente(), PDO::PARAM_INT);
+            $cont++;
+        }
+
+        if($paciente->getNombre()!=null){
+            $query->bindValue($cont, $paciente->getNombre(), PDO::PARAM_STR);
+            $cont++;
+        }
+
+        if($paciente->getApellidoPaterno()!=null){
+            $query->bindValue($cont, $paciente->getApellidoPaterno(), PDO::PARAM_STR);
+            $cont++;
+        }
+
+        if($paciente->getApellidoMaterno()!=null){
+            $query->bindValue($cont, $paciente->getApellidoMaterno(), PDO::PARAM_STR);
+            $cont++;
+        }
+
+        if($paciente->getNumTelefono()!=null){
+            $query->bindValue($cont, $paciente->getNumTelefono(), PDO::PARAM_STR);
+            $cont++;
+        }
+
+        if($paciente->getEdad()!=null){
+            $query->bindValue($cont, $paciente->getEdad(), PDO::PARAM_INT);
+            $cont++;
+        }
+
+        if($paciente->getSexo()!=null){
+            $query->bindValue($cont, $paciente->getSexo(), PDO::PARAM_STR);
+            $cont++;
+        }
+
+        if($paciente->getDireccion()!=null){
+            $query->bindValue($cont, $paciente->getDireccion(), PDO::PARAM_STR);
+        }
+
+        return $query;
+
+    }
+
+
+    public static function consultaPaciente($paciente)
+    {
+
+        $sql = Singleton::generarConsulta($paciente);
+        $query = Singleton::consultar($sql, $paciente);
+        $resultado = $query->execute();
+
+        #foreach($query->fetch(PDO::FETCH_ASSOC) as $key => $value) {
+        #    print(var_dump($key, $value).'<br>');
+        #}
+
+        return $resultado;
+
+    }
+
+
+    # ----------------------------------------------------------------
+    #                             Parto
+    # ----------------------------------------------------------------
+
+    # ----------------------------------------------------------------
+    #                             Analisis
+    # ----------------------------------------------------------------
+
 }
 
 Singleton::obtenerConexion();
-print(Singleton::iniciarSesion('toffy', '1234'));
-#Singleton::cerrarConexion();
 ?>
